f0a5bf7b4c148674d1e36ce20d9025022489c812
Fixer <67309088+leelang7@users.noreply.github.com>
2024-08-09 18:28:54 +0900

    Update 1-8.py

--- 1-8.py
+++ 1-8.py
@@ -9,7 +9,7 @@ warnings.filterwarnings(action="ignore")
 np.random.seed(100)
 
 
-def load_data(X, Y):
+def load_data(X, y):
     """1. 손글씨 데이터를 X, y로 읽어온 후
     학습 데이터, 테스트 데이터로 나눕니다.
 
@@ -18,39 +18,29 @@ def load_data(X, Y):
             X, y 데이터의 타입은 NumPy array라는 것을 참고하세요.
     """
 
-    X_train = X[:1600]
-    Y_train = Y[:1600]
+    X_train = None
+    Y_train = None
 
-    X_test = X[1600:]
-    Y_test = Y[1600:]
+    X_test = None
+    Y_test = None
 
     return X_train, Y_train, X_test, Y_test
 
 
-def train_MLP_classifier(X, Y):
+def train_MLP_classifier(X, y):
     """2. MLPClassifier를 정의하고 hidden_layer_sizes를
     조정해 hidden layer의 크기 및 레이어의 개수를
     바꿔본 후, 학습을 시킵니다.
     """
-    '''
-    coefs_: 각 층의 가중치 배열.
-    intercepts_: 각 층의 편향(바이어스) 배열.
-    n_iter_: 최적화 과정의 반복 횟수.
-    loss_: 최종 손실 값.
-    out_activation_: 출력 층의 활성화 함수 이름
-    '''
-    clf = MLPClassifier(
-        hidden_layer_sizes=(50, 50),
-        solver="adam",
-        beta_1=0.999999,
-    )
 
-    clf.fit(X, Y)
+    clf = None
+
+    None
 
     return clf
 
 
-def report_clf_stats(clf, X, Y):
+def report_clf_stats(clf, X, y):
     """3. 정확도를 출력하는 함수를 완성합니다.
     이전 실습에서 작성한 "score"를 그대로
     사용할 수 있습니다.
@@ -59,13 +49,13 @@ def report_clf_stats(clf, X, Y):
     hit = 0
     miss = 0
 
-    for x, y in zip(X, Y):
-        if clf.predict([x])[0] == y:
+    for x, y_ in zip(X, y):
+        if clf.predict([x])[0] == y_:
             hit += 1
         else:
             miss += 1
 
-    score = float(100 * (hit / (hit + miss)))
+    score = None
 
     print(f"Accuracy: {score:.1f} ({hit} hit / {miss} miss)")
 
@@ -86,21 +76,15 @@ def main():
     """
 
     digits = load_digits()
-    print(type(digits))
-    print(digits.keys())
+
     X = digits.data
-    Y = digits.target
-    print(X.shape, Y.shape)
-    print(X[0], Y[0])
+    y = digits.target
 
-    X_train, Y_train, X_test, Y_test = load_data(X, Y)
-    print(len(X_train), len(Y_train))
-    print(len(Y_test), len(Y_test))
+    X_train, Y_train, X_test, Y_test = None
 
-    clf = train_MLP_classifier(X_train, Y_train)
-    print(clf.out_activation_)
+    clf = None
 
-    score = report_clf_stats(clf, X_test, Y_test)
+    score = None
 
     return 0
 
