6a65a27c4f4b9a1da0958074c2b9fd2fbeccb9f7
Fixer <67309088+leelang7@users.noreply.github.com>
2025-03-28 20:09:16 +0900

    update

--- DL6/q4/main.py
+++ DL6/q4/main.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 from tensorflow.keras import layers, models, Sequential
 from tensorflow.keras.datasets import mnist
 import numpy as np
+import tensorflowjs as tfjs
+
 
 def load_data():
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -19,10 +21,10 @@ def load_data():
 def build_model(input_shape, num_classes=10):
     model = Sequential()
     
-    model.add(layers.Conv2D(16, kernel_size=(3, 3), padding="same", activation="relu", input_shape=input_shape))
+    model.add(layers.Conv2D(8, kernel_size=(3, 3), padding="same", activation="relu", input_shape=input_shape))
     model.add(layers.MaxPool2D(2))
     
-    model.add(layers.Conv2D(32, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(layers.Conv2D(16, kernel_size=(3, 3), padding="same", activation="relu"))
     model.add(layers.MaxPool2D(2))
     
     model.add(layers.Flatten())
@@ -30,19 +32,19 @@ def build_model(input_shape, num_classes=10):
     
     return model
 
-def main():
-    X_train, X_test, y_train, y_test = load_data()
-    
-    model = build_model(X_train[0].shape)
-    
-    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-    model.fit(X_train, y_train, epochs=1, batch_size=64, shuffle=True, verbose=2)
-    
-    # TODO: H5 형식으로 모델 저장
-    None
-    
-    # TODO: SavedModel 형식으로 모델 저장
-    None
 
-if __name__ == "__main__":
-    main()
+X_train, X_test, y_train, y_test = load_data()
+
+model = build_model(X_train[0].shape)
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.fit(X_train, y_train, epochs=1, batch_size=64, shuffle=True, verbose=1)    
+
+# TODO: 학습한 모델을 바로 변환 
+None
+
+# TODO: 다른 SavedModel 형식의 모델인 "OtherSModel"을 불러오기
+loaded_model = None
+
+# TODO: loaded_model을 tensorflow js로 변환
+None
