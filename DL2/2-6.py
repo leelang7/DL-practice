07e85032b834bbee26837480756c95def7897155
AI Diver <67309088+leelang7@users.noreply.github.com>
2025-04-16 13:01:27 +0900

    modify

--- DL2/2-6.py
+++ DL2/2-6.py
@@ -1,48 +1,108 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-'''
-1. 두 실수와 연산 종류를 입력받는 함수입니다. 코드를 살펴보세요.
-'''
 
-def insert():
-    
-    x = float(input('정수 또는 실수를 입력하세요. x : '))
-    y = float(input('정수 또는 실수를 입력하세요. y : '))
-    cal = input('어떤 연산을 할것인지 입력하세요. (+, -, *, /)')
-    
-    return x, y, cal
+np.random.seed(100)
 
 '''
-2. 입력받는 연산의 종류 cal에 따라 연산을 수행하고
-   결과를 반환하는 calcul() 함수를 완성하세요.
+1. 선형 회귀 모델의 클래스를 구현합니다.
+
+   Step01. 가중치 초기값을 1.5의 값을 가진 변수 텐서로 설정하세요.
+   
+   Step02. Bias 초기값을 1.5의 값을 가진 변수 텐서로 설정하세요.
+   
+   Step03. W, X, b를 사용해 선형 모델을 구현하세요.
 '''
 
-def calcul(x,y,cal):
-
-    result = 0
+class LinearModel:
     
-    # 더하기
-    
-    # 빼기
-    
-    # 곱하기
-    
-    # 나누기
-    
-    return result.numpy()
+    def __init__(self):
+        
+        self.W = None
+        
+        self.b = None
+        
+    def __call__(self, X, Y):
+        
+        return None
 
 '''
-3. 두 실수와 연산 종류를 입력받는 insert 함수를 호출합니다. 그 다음
-   calcul 함수를 호출해 실수 사칙연산을 수행하고 결과를 출력합니다.
+2. MSE 값을 계산해 반환하는 손실 함수를 완성합니다. 
 '''
 
+def loss(y, pred):
+    
+    return None
+
+'''
+3. gradient descent 방식으로 학습하는 train 함수입니다.
+   코드를 보면서 어떤 방식으로 W(가중치)와 b(Bias)이
+   업데이트 되는지 확인해 보세요.
+'''
+
+def train(linear_model, x, y):
+    
+    with tf.GradientTape() as t:
+        current_loss = loss(y, linear_model(x, y))
+    
+    # learning_rate 값 선언
+    learning_rate = 0.001
+    
+    # gradient 값 계산
+    delta_W, delta_b = t.gradient(current_loss, [linear_model.W, linear_model.b])
+    
+    # learning rate와 계산한 gradient 값을 이용하여 업데이트할 파라미터 변화 값 계산 
+    W_update = (learning_rate * delta_W)
+    b_update = (learning_rate * delta_b)
+    
+    return W_update,b_update
+ 
 def main():
     
-    x, y, cal = insert()
+    # 데이터 생성
+    x_data = np.linspace(0, 10, 50)
+    y_data = 4 * x_data + np.random.randn(*x_data.shape)*4 + 3
     
-    print(calcul(x,y,cal))
+    # 데이터 출력
+    plt.scatter(x_data,y_data)
+    plt.savefig('data.png')
+    
+    # 선형 함수 적용
+    linear_model = LinearModel()
+    
+    # epochs 값 선언
+    epochs = 100
+    
+    # epoch 값만큼 모델 학습
+    for epoch_count in range(epochs):
+        
+        # 선형 모델의 예측 값 저장
+        y_pred_data=linear_model(x_data, y_data)
+        
+        # 예측 값과 실제 데이터 값과의 loss 함수 값 저장
+        real_loss = loss(y_data, linear_model(x_data, y_data))
+        
+        # 현재의 선형 모델을 사용하여  loss 값을 줄이는 새로운 파라미터로 갱신할 파라미터 변화 값을 계산
+        update_W, update_b = train(linear_model, x_data, y_data)
+        
+        # 선형 모델의 가중치와 Bias를 업데이트합니다. 
+        linear_model.W.assign_sub(update_W)
+        linear_model.b.assign_sub(update_b)
+        
+        # 20번 마다 출력 (조건문 변경 가능)
+        if (epoch_count%20==0):
+            print(f"Epoch count {epoch_count}: Loss value: {real_loss.numpy()}")
+            print('W: {}, b: {}'.format(linear_model.W.numpy(), linear_model.b.numpy()))
+            
+            fig = plt.figure()
+            ax1 = fig.add_subplot(111)
+            ax1.scatter(x_data,y_data)
+            ax1.plot(x_data,y_pred_data, color='red')
+            plt.savefig('prediction.png')
 
 if __name__ == "__main__":
     main()
--- DL2/2-9.py
+++ DL2/2-9.py
@@ -1,40 +1,48 @@
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+'''
+1. 두 실수와 연산 종류를 입력받는 함수입니다. 코드를 살펴보세요.
+'''
+
+def insert():
+    
+    x = float(input('정수 또는 실수를 입력하세요. x : '))
+    y = float(input('정수 또는 실수를 입력하세요. y : '))
+    cal = input('어떤 연산을 할것인지 입력하세요. (+, -, *, /)')
+    
+    return x, y, cal
+
+'''
+2. 입력받는 연산의 종류 cal에 따라 연산을 수행하고
+   결과를 반환하는 calcul() 함수를 완성하세요.
+'''
+
+def calcul(x,y,cal):
+
+    result = 0
+    
+    # 더하기
+    
+    # 빼기
+    
+    # 곱하기
+    
+    # 나누기
+    
+    return result.numpy()
+
+'''
+3. 두 실수와 연산 종류를 입력받는 insert 함수를 호출합니다. 그 다음
+   calcul 함수를 호출해 실수 사칙연산을 수행하고 결과를 출력합니다.
+'''
+
 def main():
     
-    # XOR 문제를 위한 데이터 생성
+    x, y, cal = insert()
     
-    training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
-    target_data = np.array([[0],[1],[1],[0]], "float32")
-    
-    '''
-    1. 다층 퍼셉트론 모델을 생성합니다.
-    '''
-    
-    model = None
-    model.add(None)
-    
-    '''
-    2. 모델의 손실 함수, 최적화 방법, 평가 방법을 설정합니다.
-    '''
-    
-    model.compile(None)
-    
-    '''
-    3. 모델을 학습시킵니다. epochs를 자유롭게 설정해보세요.
-    ''' 
-    
-    hist = None
-    
-    score = hist.history['binary_accuracy'][-1]
-    
-    print('최종 정확도: ', score*100, '%')
-    
-    return hist
+    print(calcul(x,y,cal))
 
 if __name__ == "__main__":
     main()
